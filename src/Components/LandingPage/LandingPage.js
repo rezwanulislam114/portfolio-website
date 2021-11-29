b953a5bacfc5a1359919118f90c6869bd0d2bc75
rezwanulislam114 <rezwanulislam114@gmail.com>
2021-11-29 18:56:01 +0600

    typical

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from 'react'
 import { Container } from 'react-bootstrap';
-import './LandingPage.css'
+import './LandingPage.css';
+import Typical from 'react-typical'
 
 const LandingPage = () => {
     return (
         <div className="landing-page">
             <Container>
-                <h1>I'm Rezwanul Islam Rifat</h1>
-                <h2>Web Developer</h2>
+                <h1>I'm <span className="secondary-color">Rezwanul Islam Rifat</span></h1>
+                <h2>
+                    <Typical
+                        steps={[
+                            'Web Developer', 1000,
+                            'React Developer', 1000,
+                            'MERN Developer', 1000,
+                            'Web Designer', 1000,
+                        ]}
+                        loop={Infinity}
+                        wrapper="p"
+                    />
+                </h2>
                 <button className="btn-regular mt-5">
                     <i class="fas fa-download"></i>
                     <a href="https://drive.google.com/uc?export=download&id=17actcWAUKDUE7VQtl5QNR3g8jROcn6fs">Download Resume</a>
